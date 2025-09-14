e3b5d3c74b252ee75a51c5daaaab34c4ef564dbf
GURUDIPTA MANDAL <mandalgurudipta@gmail.com>
2025-09-14 18:08:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/spin-wheel-app/", // Replace with your repo name
+  base: "/Spin-Wheel/", // Replace with your repo name
 });
